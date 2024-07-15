51e8aea8c34bd779184c33ca2b7072830c53939d
chinuUpadhyay <chinup458@gmail.com>
2024-07-15 17:36:08 +0000

    conversion os datatype

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = "33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+// CONVERSION in NUMBER //
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 ;  false=> 0
+
+let isLoggedIn = "chinmay"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+// CONVERSION in BOOLEAN //
+
+// 1 => true ; 0 => false
+// "" => false
+// chinmay => true
+
+let someNumber = ""
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
