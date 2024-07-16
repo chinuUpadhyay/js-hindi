9f4a9c444a334e3d42775d6ce502f65765eb243c
chinuUpadhyay <chinup458@gmail.com>
2024-07-16 19:21:00 +0000

    datatype-summary

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "33"
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // CONVERSION in NUMBER //
 
@@ -27,8 +27,25 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = ""
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
 
 
+
+//**********************OPERATION********************//
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+let str1 = "hello"
+let str2 = " world"
+let str3 = str1+ str2
+
+console.log(str3);
+
+// console.log("2"+1+5);
+// console.log(3+"6");
+// console.log("4"+3);
+// console.log(5+5+"10");
 
---
+++ 01_basics/datatype-summary.js
@@ -0,0 +1,23 @@
+//PRIMITIV DATA TYPE//
+
+// 7 TYPE => String, Number, Boolean, Null, Symbol, BigInt..
+
+//NON PRIMITIV DATA TYPE//
+
+// 3 TYPE => Array, Object, Function..
+
+ // [Array's]
+
+ const name = ["abc", "def", "ghi" ]
+
+ // [Object]
+
+ let myObj= {
+     name: "abcd",
+     age: 17,
+ }
+
+ // [Function]
+   const myFunction = function(){
+    console.log("Hello");
+   }
